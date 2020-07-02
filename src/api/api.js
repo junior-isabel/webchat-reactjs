68854c225e9f65a1ac0a8756cfce41bb6a09cf6e
Junior Isabel <jose.junior@digitalfactory.co.ao>
2020-07-02 13:03:35 +0100

    url do servidor de api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_URL,
+    baseURL: 'https://mubenq-chat.herokuapp.com/',
     headers: {
         'content-type': 'application/json',
         'Access-Control-Allow-Origin': '*',
