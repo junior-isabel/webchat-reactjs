f1878e352f59a88b88284cfe80ddafedc2a2bd71
Junior Isabel <jose.junior@digitalfactory.co.ao>
2020-07-02 16:53:25 +0100

    adicionar endpoint do servidor

--- src/api/socket.js
+++ src/api/socket.js
@@ -3,11 +3,11 @@ import socketio from 'socket.io-client'
 export let socket
 
 export default function startSocketIO () {
-    socket = socketio('http://localhost:4000')
+    socket = socketio('https://mubenq-chat.herokuapp.com')
     return socket
 }
 export function startChat () {
-    socket = socketio('http://localhost:4000/chat')
+    socket = socketio('https://mubenq-chat.herokuapp.com/chat')
 }
 export function getSocket () {
 
