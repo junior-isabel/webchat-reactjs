d1a58fe5f3e8578e48046759c34aaf155b5ff172
jose junior <jose.j@digitalfactory.co.ao>
2020-10-20 09:43:28 +0100

    Updates

---
+++ precache-manifest.f9f68da05903b37dfb6452d5f3fa8423.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7184807bb534688541dddc9c6529c3",
+    "url": "/junior-isabel/webchat-reactjs/index.html"
+  },
+  {
+    "revision": "c70bf3a5a52c18962726",
+    "url": "/junior-isabel/webchat-reactjs/static/css/main.5f3636b4.chunk.css"
+  },
+  {
+    "revision": "dff386814231f4681396",
+    "url": "/junior-isabel/webchat-reactjs/static/js/2.ba3ceb54.chunk.js"
+  },
+  {
+    "revision": "869550218075f9cc79a88bee82abe727",
+    "url": "/junior-isabel/webchat-reactjs/static/js/2.ba3ceb54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70bf3a5a52c18962726",
+    "url": "/junior-isabel/webchat-reactjs/static/js/main.1f991b74.chunk.js"
+  },
+  {
+    "revision": "fb698a35cf7b00a847a2",
+    "url": "/junior-isabel/webchat-reactjs/static/js/runtime-main.f3e3927f.js"
+  },
+  {
+    "revision": "9235a458184fe03ecacae755c7252049",
+    "url": "/junior-isabel/webchat-reactjs/static/media/logo.9235a458.png"
+  }
+]);
--- src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import PublicRouter from './public/publicRoutes'
-import PrivateRouter from './private/privateRoutes'
-export default () =>
-    (<>
-        <PrivateRouter />
-        <PublicRouter />
-    </>)
